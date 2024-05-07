03853a0ed1d290ef49dff6cccd568d7e72f468a5
Wouter van Dam <wouter@journeyworks.nl>
2024-05-07 18:30:30 +0200

    Restore fundermaps base path .env param

--- env.d.ts
+++ env.d.ts
@@ -8,6 +8,9 @@ interface ImportMetaEnv {
   // PDOK
   readonly VITE_PDOK_LOCATIONSERVICE: string
 
+  // Fundermaps Base API Url
+  readonly VITE_FUNDERMAPS_URL: string
+  
   // Default Mapset
   readonly VITE_DEFAULT_MAPSET_ID: string
   readonly VITE_DEFAULT_LAYERS: string
