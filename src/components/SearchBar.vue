b9dad0142e915e609fe6617e3f3d11f6d2a6f955
Wouter van Dam <wouter@journeyworks.nl>
2024-05-10 12:56:13 +0200

    Search bar improvements

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import { type Ref, ref, watch } from 'vue';
 import { storeToRefs } from 'pinia';
+import mapboxgl from 'mapbox-gl';
 
 import { debouncedRef } from '@vueuse/core';
 import { vOnClickOutside } from '@vueuse/components'
-
 
 import Input from '@/components/Common/Inputs/Input.vue'
 import CloseBtn from '@/components/Common/Buttons/CloseBtn.vue';
 import IconButton from '@/components/Common/Buttons/IconButton.vue';
 import SearchIcon from '@assets/svg/icons/search.svg'
+import FundermapsIcon from './Common/Icons/FundermapsIcon.vue';
 
 import { getSuggestions, getLookup, getSuggestionsNearCoordinates } from '@/services/pdok'
 import { IPDOKSuggestion } from '@/datastructures/interfaces';
 
 import { useMainStore } from '@/store/main';
-import FundermapsIcon from './Common/Icons/FundermapsIcon.vue';
-import mapboxgl from 'mapbox-gl';
+
+import { useBuildingStore } from "@/store/buildings";
+const { setBuildingId } = useBuildingStore()
+
 const { mapCenterLatLon } = storeToRefs( useMainStore() )
 
 
@@ -26,7 +29,9 @@ const queryString = ref('')
 const debouncedQueryString = debouncedRef(queryString, 50, { maxWait: 150 } )
 
 // Results from the suggestion query
+const lastQuery: Ref<string> = ref('')
 const suggestions: Ref<IPDOKSuggestion[]> = ref([])
+const focusedSuggestion: Ref<number|null> = ref(null)
 
 // TODO: Show a no results / error message ?
 const noResults = ref(false)
@@ -42,6 +47,14 @@ watch(
 )
 
 /**
+ * Any change in the typed value directly resets the selection, without delay
+ */
+watch(
+  () => queryString.value,
+  () => focusedSuggestion.value = null
+)
+
+/**
  * Look up suggestions when focusing back on the input field
  */
 const handleFocus = async function handleFocus() {
@@ -49,15 +62,79 @@ const handleFocus = async function handleFocus() {
 }
 
 /**
+ * Change suggestion focus & selection through ArrowUp, ArrowDown & Enter keys
+ *  while focussing on the search input field
+ */
+const handleKeyDown = async function handleKeyDown(event: KeyboardEvent) {
+  
+  // Navigate with up and down keys
+  if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+    event.preventDefault()
+
+    // Start navigation
+    if (focusedSuggestion.value === null && event.key === 'ArrowDown') {
+      focusedSuggestion.value = 0
+    }
+    // End navigation
+    else if (focusedSuggestion.value === 0 && event.key === 'ArrowUp') {
+      focusedSuggestion.value = null
+    }
+
+    // Up & down
+    else if (focusedSuggestion.value !== null) {
+
+      // Detect whether we're at or past the end of the list
+      if ((focusedSuggestion.value + 1) >= suggestions.value.length) {
+        // Set to last item (override any possible issues where index exceeded length)
+        focusedSuggestion.value = suggestions.value.length - 1
+
+        // Ignore down when already at the last item
+        if (event.key === 'ArrowDown') return
+      }
+
+      if (event.key === 'ArrowDown') {
+        focusedSuggestion.value = focusedSuggestion.value + 1
+      } else {
+        focusedSuggestion.value = focusedSuggestion.value - 1
+      }
+    }
+  }
+
+  // Select through Enter key
+  else if (focusedSuggestion.value !== null && event.key === 'Enter') {
+    event.preventDefault()
+    await handleSelectBuilding(
+      suggestions.value[focusedSuggestion.value].id,
+      suggestions.value[focusedSuggestion.value].weergavenaam
+    )
+  }
+  else if (event.key === 'Escape') {
+    handleClose()
+    focusedSuggestion.value = null
+  }
+}
+
+/**
+ * When hovering over a list item in de suggestions list, mark it as the focused suggestion
+ */ 
+const onHover = function onHover(index: number) {
+  focusedSuggestion.value = index
+}
+
+/**
  * Handle getting the suggestions from PDOK
  */
 const handleGetSuggestions = async function handleGetSuggestions(query: string){
   try {
-    // remove the emptiness
+    // Remove the emptiness
     query = query.trim()
 
     // Nothing to search for
     if (query === '') return 
+
+    // Do not repeat the same query
+    if (query === lastQuery.value) return 
+    lastQuery.value = query
 
     let results
     if (mapCenterLatLon.value) {
@@ -80,6 +157,7 @@ const handleGetSuggestions = async function handleGetSuggestions(query: string){
     noResults.value = false
     suggestions.value = results.response.docs as IPDOKSuggestion[]
     
+    console.log("Suggestion results")
     console.log(results)
 
   } catch(e) {
@@ -92,28 +170,42 @@ const handleGetSuggestions = async function handleGetSuggestions(query: string){
 /**
  * The user has selected an address to view
  */
-const handleSelectBuilding = async function handleSelectBuilding(id: string) {
+const handleSelectBuilding = async function handleSelectBuilding(id: string, weergavenaam: string) {
   try {
     // console.log(id)
 
     const results = await getLookup(id)
-    console.log(results.response.docs[0])
+    // console.log("First lookup result")
+    // console.log(results.response.docs[0])
 
-    const { centroide_ll } = results.response.docs[0] // , weergavenaam
+    const { centroide_ll, nummeraanduiding_id } = results.response.docs[0] // , weergavenaam
 
-    console.log(id, centroide_ll) // , weergavenaam
+    /**
+     * Navigating to the lat lng while the API calls are being made
+     *  The lat lng from the API may differ, but that should not be a big enough difference to matter here
+     */ 
     const [ Lng, Lat ] = centroide_ll.replace('POINT(', '').replace(')', '').split(' ')
-
-    
     mapCenterLatLon.value = new mapboxgl.LngLat(Lng, Lat)
     
-    // TODO: drop pin on map
-    // TODO: open building info in right sidebar
+    // Insert the selected address in the search box, and close the suggestion list
+    lastQuery.value = weergavenaam // this prevents a Lookup API call to PDOK
+    queryString.value = weergavenaam
+    handleClose()
+
+    // Set the building id, triggering pin & sidebar
+    if (nummeraanduiding_id) {
+      setBuildingId(nummeraanduiding_id)  
+    }
 
   } catch(e) {
     console.log(e)
     error.value = true
   }
+}
+
+// What more can we do? 
+const handleSubmit = function handleSubmit() {
+  handleClose()
 }
 
 const handleClose = function handleClose() {
@@ -128,13 +220,18 @@ const handleClose = function handleClose() {
     v-on-click-outside="handleClose"
     class="relative">
 
-    <div class="search-bar | flex gap-2">
+    <form 
+      class="search-bar | flex gap-2"
+      @submit.prevent="handleSubmit">
 
       <Input 
         v-model="queryString"
         id="search" 
         placeholder="Zoek hier op adres of postcode"
-        @focus="handleFocus">
+        autocomplete="off"
+        @focus="handleFocus"
+        @keydown="handleKeyDown"
+        >
         <template v-slot:after>
           <IconButton class="text-blue-900 hover:text-green-700" transparent label="Zoeken">
             <SearchIcon 
@@ -144,7 +241,7 @@ const handleClose = function handleClose() {
         </template>
       </Input>
 
-    </div>
+    </form>
 
     <Transition>
       <div
@@ -163,7 +260,7 @@ const handleClose = function handleClose() {
               @close="handleClose" />
           </div>
           <div class="dropdown__content">
-            <ol>
+            <!-- <ol>
               <li v-for="suggestion in suggestions" :key="suggestion.id">
                 <a
                   href="#"
@@ -184,10 +281,33 @@ const handleClose = function handleClose() {
                   </div>
                 </a>
               </li>
+            </ol> -->
+            <ol>
+              <li 
+                v-for="(suggestion, index) in suggestions" :key="suggestion.id"
+                @mouseover="onHover(index)">
+                <a
+                  href="#"
+                  class="flex gap-3 px-8 py-2"
+                  :class="index === focusedSuggestion ? 'bg-grey-100 text-green-500' : ''"
+                  @click.prevent="handleSelectBuilding(suggestion.id, suggestion.weergavenaam)"
+                >
+                  <div class="flex gap-2">
+                    <span class="text-green-500"> 
+                      <FundermapsIcon
+                        name="pin"
+                        class="aspect-square w-4"
+                        aria-hidden="true" 
+                      />
+                    </span>
+                    <h6 class="heading-6 leading-none">{{ suggestion.weergavenaam }}</h6>
+                  </div>
+                </a>
+              </li>
             </ol>
           </div>
         </div>
       </div>
     </Transition>
   </div>
-</template>@/datastructures/interfaces@/services/dpok
+</template>
