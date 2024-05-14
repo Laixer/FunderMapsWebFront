fe58fa4d53cb978b4fedaf8c78b4da894704661a
Wouter van Dam <wouter@journeyworks.nl>
2024-05-14 17:57:34 +0200

    Prioritize query string LatLng over local storage

--- src/components/Mapbox/Map.vue
+++ src/components/Mapbox/Map.vue
@@ -62,7 +62,7 @@ const { maybeNudge: maybeNudgeLeft } = useMapboxControlNudge('left', 336, isLeft
 const lastKnownPositioning = getLastKnownPositioning()
 let options = ref({
   style: <string|undefined> undefined,
-  center: lastKnownPositioning.center || getLatLngFromQueryString() || [5.2913, 52.1326],
+  center: getLatLngFromQueryString() || lastKnownPositioning.center || [5.2913, 52.1326],
   zoom: lastKnownPositioning.zoom || 15,
   pitch: lastKnownPositioning.pitch || 45,
   bearing: lastKnownPositioning.bearing || 0,
