969f4cf56a37aafc4f0c46b21061914f2531dbc3
Wouter van Dam <wouter@journeyworks.nl>
2024-05-16 12:45:49 +0200

    Blocklink tweak

--- src/components/Common/Links/BlockLink.vue
+++ src/components/Common/Links/BlockLink.vue
@@ -5,18 +5,21 @@ defineProps({
   title: { type: String, required: true },
   label: { type: String, required: true }
 })
+
+const emit = defineEmits(['click'])
+
 </script>
 
 <template>
-  <a 
-    href="javascript: void;" 
-    class="link link--outline | group px-4 py-3">
+  <div
+    class="link link--outline | group px-4 py-3"
+    @click="emit('click')">
     <div>
-      <h6 class="heading-6">{{ title }}</h6>
+      <h6 class="heading-6 ">{{ title }}</h6>
       <div class="link">
         <span class="font-semibold">{{ label }}</span>
         <AnimatedArrowIcon />
       </div>
     </div>
-  </a>
+  </div>
 </template>
