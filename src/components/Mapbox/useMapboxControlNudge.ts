25f3fce94669d9045f27565bd31899285e8a8fca
Wouter van Dam <wouter@journeyworks.nl>
2024-05-23 13:49:11 +0200

    Disable nudge console logs

--- src/components/Mapbox/useMapboxControlNudge.ts
+++ src/components/Mapbox/useMapboxControlNudge.ts
@@ -7,14 +7,14 @@ export const useMapboxControlNudge = function useMapboxControlNudge(
   isOpen: Ref
 ) {
 
-  console.log("nudge - useMapboxControlNudge", side)
+  // console.log("nudge - useMapboxControlNudge", side)
 
   /**
    * Checks position, and nudges if appropiate
    */
   const maybeNudge = function nudge(){
     
-    console.log("nudge - Try nudging side", side, isOpen.value)
+    // console.log("nudge - Try nudging side", side, isOpen.value)
 
     try {
       const newOffset = isOpen.value ? `${offset}px` : '0px'
@@ -22,14 +22,14 @@ export const useMapboxControlNudge = function useMapboxControlNudge(
         // @ts-ignore - no, a DIV element _does_ have a style property
         document.getElementsByClassName(`mapboxgl-ctrl-bottom-${side}`)[0].style.setProperty(side, newOffset)
       } else {
-        console.log("nudge - No controls available at bottom position")
+        // console.log("nudge - No controls available at bottom position")
       }
 
       if (document.getElementsByClassName(`mapboxgl-ctrl-top-${side}`)?.[0]) {
         // @ts-ignore - no, a DIV element _does_ have a style property
         document.getElementsByClassName(`mapboxgl-ctrl-top-${side}`)[0].style.setProperty(side, newOffset)
       } else {
-        console.log("nudge - No controls available at top position")
+        // console.log("nudge - No controls available at top position")
       }
     } catch(e) {
       console.log("nudge - Error... we failed to nudge the mapbox controls position")
