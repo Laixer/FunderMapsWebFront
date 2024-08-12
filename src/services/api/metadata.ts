d9dbb13d9451fc6b0ab80fc6f5356f871b3c0885
Wouter van Dam <wouter@journeyworks.nl>
2024-08-12 15:59:12 +0200

    The metadata structure changed at the API

--- src/services/api/metadata.ts
+++ src/services/api/metadata.ts
@@ -5,14 +5,20 @@ import { get, put } from "../apiClient"
  * Retrieve user meta data
  */
 export const getMetadata = async function getMetadata() {
-  return await get({ endpoint: '/metadata' })
+  try {
+    const response = await get({ endpoint: '/metadata' })
+    // The actual metadata object is a stringified json object within the json response
+    return JSON.parse(response?.metadata)
+  } catch(err) {
+    return new Promise((_, reject) => reject())
+  }
 }
 
 /**
  * Store user meta data
  */
 export const setMetadata = async function setMetadata(body: object) {
-  return await put({ endpoint: '/metadata', body })
+  return await put({ endpoint: '/metadata', body: { metadata: body } })
 }
 
 export default {
