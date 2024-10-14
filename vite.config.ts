fe1bfee4338abd62014ef18891097813317e0f49
Wouter van Dam <wouter@journeyworks.nl>
2024-10-14 12:21:48 +0200

    Add git commit hash of build as version tag in <head>

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,32 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
+import { execSync } from "child_process";
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), svgLoader()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
-    ],
-  },
-  // For separate build of auth pages
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, 'index.html'),
-  //       auth: resolve(__dirname, 'auth.html'),
-  //     },
-  //   },
-  // },
+export default defineConfig(() => {
+
+  // Pass latest commit has to process environment
+  process.env.VITE_GIT_COMMIT_HASH = execSync('git rev-parse --short HEAD').toString().trimEnd()
+
+  return {
+    plugins: [vue(), svgLoader()],
+    resolve: {
+      alias: [
+        { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+        { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
+      ],
+    },
+    // For separate build of auth pages
+    // build: {
+    //   rollupOptions: {
+    //     input: {
+    //       main: resolve(__dirname, 'index.html'),
+    //       auth: resolve(__dirname, 'auth.html'),
+    //     },
+    //   },
+    // },
+  }
 })
 // vite.config.js
