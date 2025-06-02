c74540da704ddcd5dc4f7cc1ba442928d9d1b255
yorickdewid <yorickdewid@users.noreply.github.com>
2025-06-02 16:35:38 +0200

    refactor: update dependencies and adjust PostCSS configuration
    - Removed @tailwindcss/postcss from devDependencies.
    - Downgraded prettier-plugin-tailwindcss to version 0.5.13.
    - Updated tailwindcss to version 3.4.3.
    - Modified postcss.config.js to use tailwindcss/nesting and updated the tailwindcss plugin configuration.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 export default {
   plugins: {
     "postcss-import": {},
-    "@tailwindcss/postcss": {},
-    "postcss-nesting": {},
+    "tailwindcss/nesting": "",
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
